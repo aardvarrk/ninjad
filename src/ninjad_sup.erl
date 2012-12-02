1ec68d15a330eb840749c3267f6d9dd786a3fca2
Radek Slupik <daknok@icloud.com>
2012-12-02 18:05:04 +0100

    Add function to add a process supervisor
    The function is incomplete since it doesn't take enough parameters,
    but it is a start. It should take the executable specification, and
    so should ninjad_proc_sup:start_link/2.

--- src/ninjad_sup.erl
+++ src/ninjad_sup.erl
@@ -3,8 +3,16 @@
 -export([start_link/0, init/1]).
 
 start_link() ->
-    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+    {ok, Pid} = supervisor:start_link({local, ?MODULE}, ?MODULE, []),
+    add_proc_sup(Pid, 5, 10).
 
 init([]) ->
     { ok,
       { { one_for_one, 5, 10 }, [ ] } }.
+
+add_proc_sup(Sup, MaxR, MaxT) ->
+    supervisor:start_child(
+        Sup,
+        { ninjad_proc_sup,
+          { ninjad_proc_sup, start_link, [ MaxR, MaxT ] },
+          permanent, infinity, supervisor, [ ninjad_proc_sup ] } ).
