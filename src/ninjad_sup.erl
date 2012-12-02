641dff3e23753043f7ca18faee14cfa5f8867d73
Radek Slupik <daknok@icloud.com>
2012-12-02 02:48:13 +0100

    Remove useless CHILD macro
    I prefer to write complete child specifications. The macro was kinda
    limited as it assumed start_link/0.

--- src/ninjad_sup.erl
+++ src/ninjad_sup.erl
@@ -1,7 +1,6 @@
 -module(ninjad_sup).
 -behaviour(supervisor).
 -export([start_link/0, init/1]).
--define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
 
 start_link() ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
